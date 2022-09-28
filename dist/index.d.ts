12622cf7cc4863ea1b2311314f17a96f05be1e94
Anish-Shobith <>
2022-09-28 10:37:10 +0000

    build: tsc build for e3c37d2e6f956469712b94cdb44d8728e1a5154a

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
 import { Collection } from '@discordjs/collection';
-import { EventEmitter } from 'events';
+import { EventEmitter } from 'node:events';
 import WebSocket from 'ws';
 import { Pool, Dispatcher } from 'undici';
 
